4ad8214c4d403d18d71eb8d8a6cf37d877f545aa
ilkyeong aum <ilkyeong-gn@ilkyeongs-MacBook-Pro.local>
2025-05-20 10:41:44 +0900

    init: 프로젝트 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>CSS LLM Rules Test</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SomeLesson.tsx
@@ -0,0 +1,5 @@
+function SomeLesson() {
+  return <div>예제코드 복붙 후 프롬프트 테스트</div>;
+}
+
+export default SomeLesson;
